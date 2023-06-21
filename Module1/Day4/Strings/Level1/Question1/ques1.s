f3aecdb268ff95be1e26683e68fd48334435b5be
Anshuman Panda <anshumanp9920@gmail.com>
2023-06-22 02:27:48 +0530

    Moved ques1.c to Module1/Day4/Strings/Level1/Question1 along with build files.

---
+++ Module1/Day4/Strings/Level1/Question1/ques1.s
@@ -0,0 +1,142 @@
+	.file	"ques1.c"
+	.text
+	.globl	toggleCase
+	.type	toggleCase, @function
+toggleCase:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	jmp	.L2
+.L5:
+	call	__ctype_b_loc@PLT
+	movq	(%rax), %rdx
+	movq	-8(%rbp), %rax
+	movzbl	(%rax), %eax
+	movsbq	%al, %rax
+	addq	%rax, %rax
+	addq	%rdx, %rax
+	movzwl	(%rax), %eax
+	movzwl	%ax, %eax
+	andl	$512, %eax
+	testl	%eax, %eax
+	je	.L3
+	movq	-8(%rbp), %rax
+	movzbl	(%rax), %eax
+	movsbl	%al, %eax
+	movl	%eax, %edi
+	call	toupper@PLT
+	movl	%eax, %edx
+	movq	-8(%rbp), %rax
+	movb	%dl, (%rax)
+	jmp	.L4
+.L3:
+	call	__ctype_b_loc@PLT
+	movq	(%rax), %rdx
+	movq	-8(%rbp), %rax
+	movzbl	(%rax), %eax
+	movsbq	%al, %rax
+	addq	%rax, %rax
+	addq	%rdx, %rax
+	movzwl	(%rax), %eax
+	movzwl	%ax, %eax
+	andl	$256, %eax
+	testl	%eax, %eax
+	je	.L4
+	movq	-8(%rbp), %rax
+	movzbl	(%rax), %eax
+	movsbl	%al, %eax
+	movl	%eax, %edi
+	call	tolower@PLT
+	movl	%eax, %edx
+	movq	-8(%rbp), %rax
+	movb	%dl, (%rax)
+.L4:
+	addq	$1, -8(%rbp)
+.L2:
+	movq	-8(%rbp), %rax
+	movzbl	(%rax), %eax
+	testb	%al, %al
+	jne	.L5
+	nop
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	toggleCase, .-toggleCase
+	.section	.rodata
+.LC0:
+	.string	"Original string: %s\n"
+.LC1:
+	.string	"Toggled case string: %s\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movabsq	$8022916924116329800, %rax
+	movq	%rax, -21(%rbp)
+	movl	$560229490, -13(%rbp)
+	movb	$0, -9(%rbp)
+	leaq	-21(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-21(%rbp), %rax
+	movq	%rax, %rdi
+	call	toggleCase
+	leaq	-21(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	xorq	%fs:40, %rdx
+	je	.L8
+	call	__stack_chk_fail@PLT
+.L8:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
