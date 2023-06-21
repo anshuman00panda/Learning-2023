79dd8ca1f01bbb718ec8f80d0077f769b2442fce
Anshuman Panda <anshumanp9920@gmail.com>
2023-06-21 11:16:53 +0530

    Added solution of Question 2 to Module1/Day6/Level1/Question2

---
+++ Module1/Day6/Level1/Question2/ques2.s
@@ -0,0 +1,222 @@
+	.file	"ques2.c"
+	.text
+	.globl	initializeStudentArray
+	.type	initializeStudentArray, @function
+initializeStudentArray:
+.LFB6:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L3:
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	%rdx, %rax
+	salq	$3, %rax
+	subq	%rdx, %rax
+	salq	$2, %rax
+	movq	%rax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	$0, (%rax)
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	%rdx, %rax
+	salq	$3, %rax
+	subq	%rdx, %rax
+	salq	$2, %rax
+	movq	%rax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	addq	$4, %rax
+	movb	$0, (%rax)
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	%rdx, %rax
+	salq	$3, %rax
+	subq	%rdx, %rax
+	salq	$2, %rax
+	movq	%rax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	pxor	%xmm0, %xmm0
+	movss	%xmm0, 24(%rax)
+	addl	$1, -4(%rbp)
+.L2:
+	movl	-4(%rbp), %eax
+	cmpl	-28(%rbp), %eax
+	jl	.L3
+	nop
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	initializeStudentArray, .-initializeStudentArray
+	.section	.rodata
+	.align 8
+.LC1:
+	.string	"Enter the number of students: "
+.LC2:
+	.string	"%d"
+.LC3:
+	.string	"Enter the input string: "
+.LC4:
+	.string	" %[^\n]"
+.LC5:
+	.string	"\nInitialized Student Array:"
+.LC6:
+	.string	"Student %d\n"
+.LC7:
+	.string	"Roll No: %d\n"
+.LC8:
+	.string	"Name: %s\n"
+.LC9:
+	.string	"Marks: %.2f\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB7:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	addq	$-128, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-128(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC2(%rip), %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	movl	-128(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	%rdx, %rax
+	salq	$3, %rax
+	subq	%rdx, %rax
+	salq	$2, %rax
+	movq	%rax, %rdi
+	call	malloc@PLT
+	movq	%rax, -120(%rbp)
+	movl	-128(%rbp), %edx
+	movq	-120(%rbp), %rax
+	movl	%edx, %esi
+	movq	%rax, %rdi
+	call	initializeStudentArray
+	leaq	.LC3(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-112(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC4(%rip), %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	leaq	.LC5(%rip), %rdi
+	call	puts@PLT
+	movl	$0, -124(%rbp)
+	jmp	.L5
+.L6:
+	movl	-124(%rbp), %eax
+	addl	$1, %eax
+	movl	%eax, %esi
+	leaq	.LC6(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	-124(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	%rdx, %rax
+	salq	$3, %rax
+	subq	%rdx, %rax
+	salq	$2, %rax
+	movq	%rax, %rdx
+	movq	-120(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	movl	%eax, %esi
+	leaq	.LC7(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	-124(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	%rdx, %rax
+	salq	$3, %rax
+	subq	%rdx, %rax
+	salq	$2, %rax
+	movq	%rax, %rdx
+	movq	-120(%rbp), %rax
+	addq	%rdx, %rax
+	addq	$4, %rax
+	movq	%rax, %rsi
+	leaq	.LC8(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	-124(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	%rdx, %rax
+	salq	$3, %rax
+	subq	%rdx, %rax
+	salq	$2, %rax
+	movq	%rax, %rdx
+	movq	-120(%rbp), %rax
+	addq	%rdx, %rax
+	movss	24(%rax), %xmm0
+	cvtss2sd	%xmm0, %xmm0
+	leaq	.LC9(%rip), %rdi
+	movl	$1, %eax
+	call	printf@PLT
+	movl	$10, %edi
+	call	putchar@PLT
+	addl	$1, -124(%rbp)
+.L5:
+	movl	-128(%rbp), %eax
+	cmpl	%eax, -124(%rbp)
+	jl	.L6
+	movq	-120(%rbp), %rax
+	movq	%rax, %rdi
+	call	free@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L8
+	call	__stack_chk_fail@PLT
+.L8:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE7:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
