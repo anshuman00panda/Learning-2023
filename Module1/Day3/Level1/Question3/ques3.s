dc311bac5c56e86e8541d870c28286f3597353fa
Anshuman Panda <anshumanp9920@gmail.com>
2023-06-22 00:50:47 +0530

    Moved ques3.c to Module1/Day3/Level1/Question3 along with build files.

---
+++ Module1/Day3/Level1/Question3/ques3.s
@@ -0,0 +1,128 @@
+	.file	"ques3.c"
+	.text
+	.globl	findLargestNumber
+	.type	findLargestNumber, @function
+findLargestNumber:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -20(%rbp)
+	movl	$0, -12(%rbp)
+	movl	$1, -8(%rbp)
+	jmp	.L2
+.L4:
+	movl	-8(%rbp), %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	movl	%eax, %esi
+	movl	-20(%rbp), %eax
+	cltd
+	idivl	%esi
+	imull	-8(%rbp), %eax
+	movl	%eax, %ecx
+	movl	-20(%rbp), %eax
+	cltd
+	idivl	-8(%rbp)
+	movl	%edx, %eax
+	addl	%ecx, %eax
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	cmpl	-12(%rbp), %eax
+	jle	.L3
+	movl	-4(%rbp), %eax
+	movl	%eax, -12(%rbp)
+.L3:
+	movl	-8(%rbp), %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	movl	%eax, -8(%rbp)
+.L2:
+	cmpl	$1000, -8(%rbp)
+	jle	.L4
+	movl	-12(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	findLargestNumber, .-findLargestNumber
+	.section	.rodata
+.LC0:
+	.string	"Enter a 4-digit number: "
+.LC1:
+	.string	"%d"
+	.align 8
+.LC2:
+	.string	"The largest number obtained by deleting a single digit: %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-16(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	movl	-16(%rbp), %eax
+	movl	%eax, %edi
+	call	findLargestNumber
+	movl	%eax, -12(%rbp)
+	movl	-12(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC2(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	xorq	%fs:40, %rdx
+	je	.L8
+	call	__stack_chk_fail@PLT
+.L8:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
